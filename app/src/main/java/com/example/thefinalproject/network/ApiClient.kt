04be753d70a67a37a4a0447bdb25ed1f297977e9
MuhammadIsmailALFarouk <ismail.gariskeras@gmail.com>
2023-11-22 00:50:57 +0700

    Set Up Project

---
+++ app/src/main/java/com/example/thefinalproject/network/ApiClient.kt
@@ -0,0 +1,4 @@
+package com.example.thefinalproject.network
+
+object ApiClient {
+}
--- app/src/main/java/com/example/thefinalproject/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/thefinalproject/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.thefinalproject
+package com.example.thefinalproject.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.thefinalproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
